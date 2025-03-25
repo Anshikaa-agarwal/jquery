65352652091acbbd9046f20c3d56bcbac802f8e5
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-03-25 18:14:40 +0530

    basic routing

--- express/index.js
+++ express/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.send("helloo world");
+    res.statusCode(200)
 })
 
 app.get('/about', (req, res) => {
-    res.send("about page")
+    res.sendFile(path.join(__dirname, 'complex.html'))
+})
+
+app.get('/blog', (req, res) => {
+    res.send("blogs page here!")
+})
+app.get('/blog/:slug', (req,res) => {
+    res.send(`Heyyy ${req.params.slug}`)
 })
 
 app.listen(port, () => {
